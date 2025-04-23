81b84e8ee0b65586ae570bff19b526b9782211c8
Haadia <hmufti.24@gamil.com>
2025-04-23 12:43:01 -0400

    added a comment

--- carbohydrate_calculator/src/carbohydrate_calculator/TestCarbCalculator.java
+++ carbohydrate_calculator/src/carbohydrate_calculator/TestCarbCalculator.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
 public class TestCarbCalculator {
+	// This test opens the carbohydrate calculator page on the Chrome browser and 
+	// validates if the page heading has appeared
 	WebDriver driver;
 
 	@Test
@@ -17,6 +19,7 @@ public class TestCarbCalculator {
 		driver.get("https://www.calculator.net/carbohydrate-calculator.html");
 		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
 		boolean titleShown = driver.getTitle().contains("Carbohydrate Calculator");
+		// Chekc if "Carbohydrate Calculator" appears on the page title
 		Assert.assertTrue(titleShown, "Failed to show page header");
 	}
 	
